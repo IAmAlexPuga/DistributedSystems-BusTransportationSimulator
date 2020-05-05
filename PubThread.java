c3ab2d71180b9a4add5f287841b4b246bc3a0c73
Alex Puga <alex.puga@wsu.edu>
2020-05-04 17:57:53 -0700

    Added Accident variables and Init funcs

--- PubThread.java
+++ PubThread.java
@@ -10,26 +10,26 @@ import com.rti.dds.infrastructure.StatusKind;
 import com.rti.dds.publication.Publisher;
 import com.rti.dds.topic.Topic;
 
-
-public class PubThread extends Thread
-{
+public class PubThread extends Thread {
 	Position p = new Position();
 	LocalDateTime now;
 	DateTimeFormatter dtf;
 	int Cycles = 1;
-	
+
 	PositionPublisher pos;
 	AccidentPublisher accident;
-	
-	
+
+	// Positoins
 	DomainParticipant participant = null;
-    Publisher publisher = null;
-    Topic topic = null;
-    PositionDataWriter writer = null;
-    InstanceHandle_t instance_handle = InstanceHandle_t.HANDLE_NIL;
+	PositionDataWriter writer = null;
+
+	// Accidents
+	DomainParticipant a_participant = null;
+	AccidentDataWriter a_writer = null;
 	
-	public PubThread(Position pos, LocalDateTime time)
-	{
+	InstanceHandle_t instance_handle = InstanceHandle_t.HANDLE_NIL;
+
+	public PubThread(Position pos, LocalDateTime time) {
 
 		p.fillInRation = pos.fillInRation;
 		p.numStops = pos.numStops;
@@ -39,167 +39,196 @@ public class PubThread extends Thread
 		p.timestamp = pos.timestamp;
 		p.trafficConditions = pos.trafficConditions;
 		p.vehicle = pos.vehicle;
-		
+
 		this.now = time;
 		this.dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
 		this.pos = new PositionPublisher();
 		this.accident = new AccidentPublisher();
 		Random rand = new Random();
-		
-		
-	    
-		participant = DomainParticipantFactory.TheParticipantFactory.create_participant(0, DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT,null /* listener */, StatusKind.STATUS_MASK_NONE);
-		if (participant == null) 
-		{
-            System.err.println("create_participant error\n");
-            return;
-        }
-		
-		publisher = participant.create_publisher(DomainParticipant.PUBLISHER_QOS_DEFAULT, null /* listener */,StatusKind.STATUS_MASK_NONE);
-        if (publisher == null) 
-        {
-        	
-            System.err.println("create_publisher error\n");
-            return;
-        }
-        
-        String typeName = PositionTypeSupport.get_type_name();
-        PositionTypeSupport.register_type(participant, typeName);
 
-        topic = participant.create_topic("Example Position",typeName, DomainParticipant.TOPIC_QOS_DEFAULT,null /* listener */, StatusKind.STATUS_MASK_NONE);
-        if (topic == null) {
-            System.err.println("create_topic error\n");
-            return;
-        }           
+		InitPosVars();
 
-        writer = (PositionDataWriter)publisher.create_datawriter(topic, Publisher.DATAWRITER_QOS_DEFAULT,null /* listener */, StatusKind.STATUS_MASK_NONE);
-        if (writer == null)
-        {
-            System.err.println("create_datawriter error\n");
-            return;
-        }           
-        
 		System.out.println("Thread " + p.vehicle + " Started");
 	}
-	
-	public void run()
-	{
-		
-		try
-		{
+
+	public void InitAccVars() {
+		DomainParticipant participant = null;
+		Publisher publisher = null;
+		Topic topic = null;
+		AccidentDataWriter writer = null;
+
+		participant = DomainParticipantFactory.TheParticipantFactory.create_participant(0,DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT, null /* listener */, StatusKind.STATUS_MASK_NONE);
+		if (participant == null) {
+
+			System.err.println("create_participant error\n");
+			return;
+		}
+
+		publisher = participant.create_publisher(DomainParticipant.PUBLISHER_QOS_DEFAULT, null /* listener */,StatusKind.STATUS_MASK_NONE);
+		if (publisher == null) {
+			System.err.println("create_publisher error\n");
+			return;
+		}
+
+		String typeName = AccidentTypeSupport.get_type_name();
+		AccidentTypeSupport.register_type(participant, typeName);
+
+
+		topic = participant.create_topic("Example Accident", typeName, DomainParticipant.TOPIC_QOS_DEFAULT,null /* listener */, StatusKind.STATUS_MASK_NONE);
+		if (topic == null) {
+			System.err.println("create_topic error\n");
+			return;
+		}
+
+		a_writer = (AccidentDataWriter) publisher.create_datawriter(topic, Publisher.DATAWRITER_QOS_DEFAULT,null /* listener */, StatusKind.STATUS_MASK_NONE);
+		if (writer == null) {
+			System.err.println("create_datawriter error\n");
+			return;
+		}
+
+	}
+
+	public void InitPosVars() {
+		Publisher publisher = null;
+		Topic topic = null;
+
+		participant = DomainParticipantFactory.TheParticipantFactory.create_participant(0,
+				DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT, null /* listener */, StatusKind.STATUS_MASK_NONE);
+		if (participant == null) {
+			System.err.println("create_participant error\n");
+			return;
+		}
+
+		publisher = participant.create_publisher(DomainParticipant.PUBLISHER_QOS_DEFAULT, null /* listener */,
+				StatusKind.STATUS_MASK_NONE);
+		if (publisher == null) {
+
+			System.err.println("create_publisher error\n");
+			return;
+		}
+
+		String typeName = PositionTypeSupport.get_type_name();
+		PositionTypeSupport.register_type(participant, typeName);
+
+		topic = participant.create_topic("Example Position", typeName, DomainParticipant.TOPIC_QOS_DEFAULT,
+				null /* listener */, StatusKind.STATUS_MASK_NONE);
+		if (topic == null) {
+			System.err.println("create_topic error\n");
+			return;
+		}
+
+		writer = (PositionDataWriter) publisher.create_datawriter(topic, Publisher.DATAWRITER_QOS_DEFAULT,null /* listener */, StatusKind.STATUS_MASK_NONE);
+		if (writer == null) {
+			System.err.println("create_datawriter error\n");
+			return;
+		}
+	}
+
+	public void run() {
+
+		try {
 			Random rand = new Random();
 			LocalDateTime old;
 			int departing = 0;
 
-			while(Cycles < 4)
-			{
-				if(p.stopNumber > p.numStops)
-				{
-					Cycles ++;
+			while (Cycles < 4) {
+				if (p.stopNumber > p.numStops) {
+					Cycles++;
 					p.stopNumber = 1;
 				}
 				old = this.now;
 				SetTraffic(rand.nextInt(101) + 1);
 				CheckAccident(rand.nextInt(101) + 1);
-				if(p.fillInRation > 0)
-				{
+				if (p.fillInRation > 0) {
 					departing = rand.nextInt(p.fillInRation);
-				}else {
+				} else {
 					departing = 0;
 				}
-				
-				Stop(rand.nextInt(100 - p.fillInRation) - departing, GetTimeStampBetween(old));
-				p.stopNumber ++;
-			}
-		}finally {
-			 if(participant != null) {
-	                participant.delete_contained_entities();
 
-	                DomainParticipantFactory.TheParticipantFactory.
-	                delete_participant(participant);
-	            }
-		}	
+				Stop(rand.nextInt(100 - p.fillInRation) - departing, GetTimeStampBetween(old));
+				p.stopNumber++;
+			}
+		} finally {
+			if (participant != null) {
+				participant.delete_contained_entities();
+
+				DomainParticipantFactory.TheParticipantFactory.delete_participant(participant);
+			}
+		}
 	}
-	
+
 	// Sets the traffic and ads the set time based on traffic
-	public void SetTraffic(int chance)
-	{
-		if(chance < 26)
-		{
+	public void SetTraffic(int chance) {
+		if (chance < 26) {
 			p.trafficConditions = "Light";
 			SetTime((long) (p.timeBetweenStops - (.25 * p.timeBetweenStops)));
-		}else if(chance > 89)
-		{
+		} else if (chance > 89) {
 			p.trafficConditions = "Heavy";
 			SetTime((long) (1.5 * p.timeBetweenStops));
-		}else {
+		} else {
 			p.trafficConditions = "Normal";
 			SetTime((long) (p.timeBetweenStops));
 		}
 
 	}
-	
+
 	// Adds nanosec to old time
-	public void SetTime(long nanosec)
-	{
-		LocalDateTime time = now.plusNanos(1000000000*nanosec);
+	public void SetTime(long nanosec) {
+		LocalDateTime time = now.plusNanos(1000000000 * nanosec);
 		time.format(dtf);
 		now = time;
 	}
-	
+
 	// Checks if an accident has occured
-	public void CheckAccident(int chance)
-	{
-		if(chance < 11)
-		{
+	public void CheckAccident(int chance) {
+		if (chance < 11) {
 			SetTime(10);
 			p.timestamp = now.toString();
 			PostAccident();
 		}
 	}
-	
+
 	// Returns the time difference between the current and old time
-	public String GetTimeStampBetween(LocalDateTime old)
-	{
+	public String GetTimeStampBetween(LocalDateTime old) {
 		String res = "";
 		LocalDateTime temp = LocalDateTime.from(old);
-		
+
 		long time = temp.until(this.now, ChronoUnit.HOURS);
 		temp = temp.plusHours(time);
 		res += time + ":";
-		
+
 		time = temp.until(this.now, ChronoUnit.MINUTES);
 		temp = temp.plusMinutes(time);
 		res += time + ":";
-		
+
 		time = temp.until(this.now, ChronoUnit.SECONDS);
 		temp = temp.plusSeconds(time);
 		res += time;
-		
+
 		return res;
 	}
-	
+
 	// Posts an accident
-	synchronized  public void PostAccident()
-	{
+	synchronized public void PostAccident() {
 		Accident acc = new Accident();
 		acc.stopNumber = p.stopNumber;
 		acc.route = p.route;
 		acc.timestamp = p.timestamp;
 		acc.vehicle = p.vehicle;
-		
-		//accident.SetAccident(acc);
-		System.out.println(p.vehicle + " published a accident message at stop # " + p.stopNumber + " on route " + p.route + "at " + p.timestamp);
 
-	}
-	
-	// post a stop
-	synchronized public void Stop(int boarding, String time)
-	{
-		p.fillInRation += boarding;
-		System.out.println(p.vehicle + " published a position message at stop # " + p.stopNumber + " on route " + p.route + "at " + p.timestamp);
+		System.out.println(p.vehicle + " published a accident message at stop # " + p.stopNumber + " on route "
+				+ p.route + " at " + p.timestamp);
 		
-		writer.write(p,instance_handle);
+		a_writer.write(acc, instance_handle);
 	}
-	
+
+	// post a stop
+	synchronized public void Stop(int boarding, String time) {
+		p.fillInRation += boarding;
+		System.out.println(p.vehicle + " published a position message at stop # " + p.stopNumber + " on route "
+				+ p.route + "at " + p.timestamp);
+
+		writer.write(p, instance_handle);
+	}
+
 }
